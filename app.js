852065e0bf10d1af994bb45d0757603cc4b51ace
15744 <1574408801@qq.com>
2019-07-25 18:27:11 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+﻿const express = require("express");                            //引入express框架
+const mobileRouter = require('./controller/mobileRouter');    //引入移动端客户路由
+const session = require("express-session");                  //引入session模块
+
+
+const app = express();                                   //实例化express
+
+
+//引入中间件
+app.use(session({                                     //使用session中间件
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.static("./public"));                             //静态PUBLIC
+app.use(express.static("./static"));                             //静态static
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
+
+
+//移动端部分
+app.post("/app/getPlantWideLoad", mobileRouter.getPlantWideLoad);
+app.post("/app/getPlantWideLoadRate", mobileRouter.getPlantWideLoadRate);
+app.post("/app/getPlantWideElectricQuantity", mobileRouter.getPlantWideElectricQuantity);
+app.post("/app/getPlantWideCrewRate", mobileRouter.getPlantWideCrewRate);
+
+
+
+app.listen(3001);                                                 //监听3000端口
+
+console.log("SERVER START");                                     //控制台打印服务器成功启动信息
---
+++ controller/mobileRouter.js
@@ -0,0 +1,46 @@
+﻿var formidable = require("formidable");                         //引入formidable
+var mongodb = require("../models/mongodb");                    //引入models操作数据模块
+var md5 = require("../models/md5");                           //引入md5加密
+
+
+let  jizugonglv = [
+    {"number":"246","load":"mw","name":"#1号机组"},
+    {"number":"247","load":"mw","name":"#2号机组"},
+    {"number":"248","load":"mw","name":"#3号机组"},
+    {"number":"249","load":"mw","name":"#4号机组"},
+    {"number":"250","load":"mw","name":"#5号机组"}
+];
+
+
+exports.getPlantWideLoad = function (req, res, next) {
+    res.json({
+        "state":"1",
+        "message":"请求成功",
+        data:{"data":"1487"}
+    })
+};
+
+exports.getPlantWideLoadRate = function (req, res, next) {
+    res.json({
+        "state":"1",
+        "message":"请求成功",
+        "data":{"data":"74"}
+    })
+};
+
+exports.getPlantWideElectricQuantity = function (req, res, next) {
+    res.json({
+        "state":"1",
+        "message":"请求成功",
+        "data":{"data":"2487"}
+    })
+};
+
+exports.getPlantWideCrewRate = function (req, res, next) {
+    res.json({
+        "state":"1",
+        "message":"请求成功",
+        "data":jizugonglv
+    })
+};
+
